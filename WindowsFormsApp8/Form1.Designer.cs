ed7d4fcb479d7c67851a5d5826105209c61f02b3
MKTunca <91753517+MKTunca@users.noreply.github.com>
2021-12-27 00:20:36 +0300

    Add files via upload

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ad = new System.Windows.Forms.TextBox();
+            this.soyad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ad
+            // 
+            this.ad.Location = new System.Drawing.Point(43, 62);
+            this.ad.Name = "ad";
+            this.ad.Size = new System.Drawing.Size(100, 20);
+            this.ad.TabIndex = 0;
+            // 
+            // soyad
+            // 
+            this.soyad.Location = new System.Drawing.Point(197, 62);
+            this.soyad.Name = "soyad";
+            this.soyad.Size = new System.Drawing.Size(100, 20);
+            this.soyad.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(334, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "sonuç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.soyad);
+            this.Controls.Add(this.ad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ad;
+        private System.Windows.Forms.TextBox soyad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a1 = Convert.ToString(ad.Text);
+            string a2 = Convert.ToString(soyad.Text);
+            string birlesme = toplam(a1, a2);
+            MessageBox.Show(birlesme);
+        }
+        public string toplam(string a, string b)
+        {
+            string sonuc = "";
+            sonuc=a+" "+b;
+            return sonuc;
+        }
+
+
+    }
+}
